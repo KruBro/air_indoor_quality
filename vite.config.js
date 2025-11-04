54dcb2516af7eeac619d67f47aa53cbd91a815c2
KruBro <techkru697@gmail.com>
2025-11-05 00:58:25 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Add this line, using your exact repo name:
+  base: '/air_indoor_qaulity/', 
   plugins: [react()],
 })
